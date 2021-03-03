0fe0d2b5f71e48ff819181b0b311df49b5f5b350
Maria Fernanda Chavez <mf.chavezminjarez@gmail.com>
2021-03-03 17:44:50 +0000

    Add general stats to admin dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,11 @@
 class Admin::DashboardController < ApplicationController
-  http_basic_authenticate_with name: ENV["ADMIN_USERNAME"], password: ENV["ADMIN_PASSWORD"]
+  # http_basic_authenticate_with name: ENV["ADMIN_USERNAME"], password: ENV["ADMIN_PASSWORD"]
 
   def show
+    @product_count = Product.count
+    @category_count = Product.group(:category_id).count
   end
 end
+
+
+# Product.joins(:category).group(:category).count
