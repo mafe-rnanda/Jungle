3c1f17e433cfb46f426d7cb3b3158b2f7d7c5c76
Maria Fernanda Chavez <mf.chavezminjarez@gmail.com>
2021-03-04 20:11:56 +0000

    Add user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root to: 'products#index'
 
-  get '/about' => 'about#index'
+  get '/about', to: 'about#index'
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
@@ -19,6 +19,13 @@ Rails.application.routes.draw do
     resources :products, except: [:edit, :update, :show]
     resources :categories, only: [:index, :new, :create]
   end
+  
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+  
+  get '/signup', to: 'users#new'
+  post '/users', to: 'users#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
